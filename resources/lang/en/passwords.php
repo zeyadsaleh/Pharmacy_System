ebad17707b63da90877a9f80fe9eded542003329
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-04-05 15:59:10 +0200

    reset user password

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -18,5 +18,8 @@ return [
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
+    'timeout' => 'Please wait before retrying.',
+
+
 
 ];
