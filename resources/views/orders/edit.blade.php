f78b9fb3bbbc7a19ee89a86482c4cf130abb1866
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-04-07 15:44:37 +0200

    Organizing the status's flow of order

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -1,78 +1,89 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Order')
 
 @section('css')
   @include('layouts.ordercss')
 @stop
 
 @section('content_header')
-
 @stop
 
 @section('content')
 <div class="container d-flex justify-content-center">
-       <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+       <div class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 border rounded p-5">
            <div class="panel panel-info" >
-                   <div class="panel-heading">
-                       <h2 class="panel-title">Order Info</h2>
+                   <div class="panel-heading text-center shadow rounded">
+                       <h2 class="panel-title border bg-dark mb-4 p-auto">Order Info</h2>
                    </div>
-                   <div style="padding-top:30px" class="panel-body" >
+                   <div class="panel-body pt-3" >
 
                        <form id="order" method="POST" action="{{route('orders.update',['order' => $order->id])}}" enctype="multipart/form-data" class="form-horizontal" role="form">
                          @method('PUT')
                          @csrf
                            <p><b>Order Username</b></p>
-                           <div style="margin-bottom: 25px" class="input-group">
-                                       <input type="text" class="form-control" name="username" value="{{$order->user->name}}" disabled>
+                           <div class="input-group">
+                                       <input type="text" class="form-control" name="username" value="{{$order->user->name}}" readonly>
                                    </div>
 
-                          <hr>
+                           <hr>
 
                            <p><b>Deliverying Address</b></p>
-                           <div style="margin-bottom: 25px" class="input-group">
-                                       <input type="text" class="form-control" name="addres" value="{{$order->delivering_address}}" disabled>
+                           <div class="input-group">
+                                       <input type="text" class="form-control" name="addres" value="{{$order->delivering_address}}" readonly>
                            </div>
 
                            <hr>
 
-                            <p><b>Creation Data</b></p>
-                            <div style="margin-bottom: 25px" class="input-group">
-                                        <input type="text" class="form-control" name="creation_data" value="{{$order->created_at}}" disabled>
+                            <p><b>Created by</b></p>
+                            <div class="input-group">
+                                        <input type="text" class="form-control" name="creation_data" value="{{$order->created_by}}" readonly>
                             </div>
 
                             <hr>
 
-                             <p><b>Current Order Status: </b><span style="color: green;"><u>{{$order->status}}</u></span></p>
-
+                            <p><b>Doctor Name</b></p>
                             <div style="margin-bottom: 25px" class="input-group">
+                                        <input type="text" class="form-control" name="dname" value="{{$order->doctor? 'Dr. '.$order->doctor->name : " "}}" readonly>
+                                    </div>
+
+                           <hr>
+
+                           <p><b>Is Insured</b></p>
+                           <div class="input-group">
+                                       <input type="text" class="form-control" name="isinsured" value="{{$order->is_insured? 'Yes':'No'}}" readonly>
+                                   </div>
+
+                          <hr>
+
+                            <p><b>Current Order Status: </b><span style="color: green;"><u>{{$order->status}}</u></span></p>
+                            <div class="input-group">
                                  <select name="status" class="form-control">
                                      <option value="{{$order->status}}">{{$order->status}}</option>
-                                   @if($order->status != 'New')
-                                     <option value="New">New</option>
-                                   @endif
-                                   @if($order->status != 'Processing')
-                                     <option value="Processing">Processing</option>
-                                   @endif
-                                   @if($order->status != 'WaitingForUserConfirmation')
-                                     <option value="WaitingForUserConfirmation">WaitingForUserConfirmation</option>
-                                   @endif
-                                   @if($order->status != 'Canceled')
+                                     @php ($i = $order->status)
+                                     @switch($i)
+                                       @case('New')
+                                         <option value="Processing">Processing</option>
+                                         @php ($i = 'Processing')
+                                       @case('Processing')
+                                         <option value="WaitingForUserConfirmation">WaitingForUserConfirmation</option>
+                                         @php ($i = 'WaitingForUserConfirmation')
+                                       @case('WaitingForUserConfirmation')
+                                        <option value="Delivered">Delivered</option>
+                                     @endswitch
+
+                                      @if($order->status == 'Caneled')
                                      <option value="Canceled">Canceled</option>
-                                   @endif
-                                   @if($order->status != 'Confirmed')
                                      <option value="Confirmed">Confirmed</option>
-                                   @endif
-                                   @if($order->status != 'Delivered')
                                      <option value="Delivered">Delivered</option>
                                    @endif
                                 </select>
                             </div>
 
-                         <div style="margin-top:10px" class="form-group">
+                         <div class="form-group">
                              <!-- Button -->
-                             <div class="col-sm-12 controls">
-                               <button type="submit" class="btn btn-success">Edit</button>
+                             <div class="d-flex justify-content-center mt-5">
+                                 <button class="btn btn-lg btn-success shadow-lg">Edit iT!</button>
                              </div>
                          </div>
 
