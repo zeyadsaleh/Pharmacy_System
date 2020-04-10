60dd8dbb40d88aab49c3d1219b2d39a51a325654
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-04-10 15:37:04 +0200

    client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Client;
 use App\Http\Requests\StoreClientRequest;
+use App\Http\Resources\ClientResource;
+use App\User;
+use Dotenv\Exception\ValidationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Yajra\DataTables\Facades\DataTables;
 
 
@@ -18,17 +23,11 @@ class ClientController extends Controller
     public function index(Request $request)
     {
         if ($request->ajax()) {
-            $clients = Client::all();
-            return DataTables::of($clients)
-                ->addColumn('action', function ($clients) {
-                    $button = '<a type="button" name="edit" href="clients/' . $clients->id . '/edit" class="edit btn btn-primary btn-sm">Edit</a>';
-                    $button .= "&nbsp;&nbsp;&nbsp";
-                    $button .= "<form method='POST' id='delete-$clients->id' class='d-inline' action=" . route('admin.clients.destroy', ['client' => $clients->id]) . "><input type='hidden' name='_token' value='" . csrf_token() . "'><input type='hidden' name='_method' value='DELETE'>";
-                    $button .= "<button type='button' onclick='deleteArea($clients->id)' data-id='$clients->id' class='btn mx-1 btn-danger btn-sm'>Delete</button></form>";
-                    return $button;
-                })
-                ->rawColumns(['action'])
+            if ($request->ajax()) {
+                return Datatables::of(ClientResource::collection(Client::all()))
                 ->make(true);
+            }
+            return view('userAddresses.index');
         }
         return view('clients.index');
     }
@@ -40,23 +39,86 @@ class ClientController extends Controller
     public function store(StoreClientRequest $request)
     {
         Client::create($request->validated());
+        $validatedData = $request->validated();
+
+        if ($request->hasfile('avatar')) {
+            $file = $request->file('avatar');
+            $originalName = $file->getClientOriginalName(); // original name of file
+            $extension = $file->getClientOriginalExtension(); // getting image extension
+            $filename = time() . '.' . $extension;
+
+            $file->move('avatars/', $filename);
+        }
+
+        $validatedData['avatar'] = '/' . $filename;
+
+        $user = User::create([
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password'])
+        ]);
+
+        $client = Client::create([
+            'name' => $validatedData['name'],
+            'gender' => $validatedData['gender'],
+            'date_of_birth' => $validatedData['date_of_birth'],
+            'national_id' => $validatedData['national_id'],
+            'avatar' => $validatedData['avatar'],
+            'avatar_file_name' => $originalName,
+            'mobile_number' => $validatedData['mobile_number'],
+            'is_insured' => false
+        ]);
+
+        $user->assignRole('client');
+
+        $client->user()->save($user);
         return redirect()->route('admin.clients.index');
     }
 
     public function edit(Request $request)
     {
         $client = Client::find($request->client);
+        $email = User::select('email')->where('profile_id',$client->id)->first()->email;
         return view('clients.edit', [
-            'client' => $client
+            'client' => $client,
+            'email' => $email
         ]);
     }
     public function update(Request $request)
     {
-        Client::where('id', $request->cleint)->update([
-            'name' => $request->name,
-            'email' => $request->email,
-        ]);
-        return redirect()->route('admin.cleints.index');
+        if (auth()->user()->id ==  $request->client && auth()->user()->hasRole('client')) {
+            if ($request->has('email')) {
+
+            }
+
+            if ($request->hasfile('avatar')) {
+                $file = $request->file('avatar');
+                $originalName = $file->getClientOriginalName(); // original name of file
+                $extension = $file->getClientOriginalExtension(); // getting image extension
+                $filename = time() . '.' . $extension;
+                $file->move('avatars/', $filename);
+            }
+
+            $client = Client::find($request->client);
+
+            $client->update([
+                'name' => $request->name ? $request->name : $client->name,
+                'gender' => $request->gender ? $request->gender : $client->gender,
+                'date_of_birth' => $request->date_of_birth ? $request->date_of_birth : $client->date_of_birth,
+                'national_id' => $request->national_id ? $request->national_id : $client->national_id,
+                'avatar' => $request->avatar ? '/' . $filename : $client->avatar,
+                'avatar_file_name' => $request->avatar ? $originalName : $client->avatar_file_name,
+                'mobile_number' => $request->mobile_number ? $request->mobile_number : $client->mobile_number,
+            ]);
+
+            $client->user()->update([
+                'password' => $request->password ? Hash::make($request->password) : $client->user->password,
+            ]);
+            // Client::where('id', $request->client)->update([
+            //     'name' => $request->name,
+            //     'email' => $request->email,
+            // ]);
+             return redirect()->route('admin.cleints.index');
+        }
     }
 
     public function destroy()
--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -14,36 +14,72 @@
 
 @section('content')
 <div class="container">
-@if ($errors->any())
+    @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+            <li>{{ $error }}</li>
             @endforeach
         </ul>
     </div>
-@endif
-<form method="POST" action="{{route('admin.clients.store')}}" enctype="multipart/form-data">
-    @csrf
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="name">Name</label>
-      <input type="text" name="name" class="form-control" id="name" placeholder="Name">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="address"></label>
-      <input type="text" name="address" class="form-control" id="address" placeholder="Address">
-    </div>
-  <button type="submit" class="btn btn-primary">Add</button>
-</form>
-</div>
-@stop
+    @endif
+    <form method="POST" action="{{route('admin.clients.store')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="name">Name</label>
+                <input type="text" name="name" class="form-control" id="name" placeholder="Name">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="email">e-mail</label>
+                <input type="email" name="email" class="form-control" id="email" placeholder="email">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="password">password</label>
+                <input type="password" name="password" class="form-control" id="password" placeholder="password">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="password_confirmation">confirm password</label>
+                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation"
+                    placeholder="Confirm Password">
+            </div>
+            <div class="form-group">
+                <label for="gender">Gender</label>
+                <select name="gender" class="form-control" id="gender">
+                    <option value="Female">Female</option>
+                    <option value="Male">Male</option>
+                </select>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="national_id">National ID</label>
+                <input type="text" name="national_id" class="form-control" id="national_id" placeholder="national_id">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="mobile_number">Mobile Number</label>
+                <input type="text" name="mobile_number" class="form-control" id="mobile_number"
+                    placeholder="mobile number">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="date_of_birth">Birth Date</label>
+            <input type="date" id="date_of_birth" name="date_of_birth" min="2018-01-01" max="2020-04-11">
+            </div>
+            <div class="form-group col-md-4">
+                <div class="form-group col-md-12">
+                    <label for="avatar">Image</label>
+                    <input type="file" class="d-block" id="avatar" name="avatar" accept="image/*">
+                </div>
+            </div>
 
-@section('css')
+        </div>
+        <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+    @stop
+
+    @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
-@stop
+    @stop
 
-@section('js')
+    @section('js')
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-@stop
+    @stop
